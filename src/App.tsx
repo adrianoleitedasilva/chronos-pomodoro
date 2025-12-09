1134dc7655d1a0c197f736c8774b8bc2f559bc93
Adriano Leite da Silva <adrianoleitedasilva@outlook.com>
2025-12-09 17:27:43 -0300

    create: adicionando o componente Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -12,7 +13,7 @@ export function App() {
       </Container>
 
       <Container>
-        <section>Menu</section>
+        <Menu />
       </Container>
 
       <Container>
