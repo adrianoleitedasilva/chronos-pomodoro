79b94ffd90e01ffac2cd3a00921f33832a460da2
Adriano Leite da Silva <adrianoleitedasilva@outlook.com>
2025-12-12 15:57:52 -0300

    create: criando o componente Form

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,25 @@ export function App() {
       </Container>
 
       <Container>
-        <section>Form</section>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <label htmlFor='input'>Tarefa</label>
+            <input type='text' id='input' />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0 </p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
 
       <Container>
