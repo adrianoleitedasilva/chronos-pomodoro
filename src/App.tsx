921115a7b0057959b0cc3cc22ffcb1ebb3f3b5ae
Adriano Leite da Silva <adrianoleitedasilva@outlook.com>
2025-12-12 20:50:19 -0300

    create: criando o componente reutilizavel dos botoes

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { Menu } from './components/Menu';
 import './styles/theme.css';
 import './styles/global.css';
 import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
 
 export function App() {
   return (
@@ -43,7 +45,7 @@ export function App() {
           </div>
 
           <div className='formRow'>
-            <button>Enviar</button>
+            <DefaultButton icon={<PlayCircleIcon />} color='green' />
           </div>
         </form>
       </Container>
